563d92e8d2cc1f2f942c75c8fc429325a2d69c9a
Taha Afzal <tahaafzal5@hotmail.com>
2020-10-21 09:39:58 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Taha Afzal on 10/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
