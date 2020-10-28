3bbe92bb71f41953c7fb8d46658dcc481980feac
Taha Afzal <tahaafzal5@hotmail.com>
2020-10-28 18:35:44 -0500

    Initial working of Comments

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -73,6 +73,27 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            }
+            else {
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+    
 
     /*
     // MARK: - Navigation
